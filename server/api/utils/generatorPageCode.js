a2322659b8ea221fc2a46580f04b23d1f2a61770
joel <iamjoel007@gmail.com>
2018-06-08 10:36:17 +0800

    can almost generator code

--- server/api/utils/generatorPageCode.js
+++ server/api/utils/generatorPageCode.js
@@ -1,7 +1,11 @@
 module.exports = function(config) {
-  var vue = generatorVue(config)
-  var js = generatorJS(config)
-  var css = generatorCSS(config)
+  try {
+    var vue = generatorVue(config)
+    var js = generatorJS(config)
+    var css = generatorCSS(config)
+  } catch(e) {
+    console.log(e)
+  }
 
   return {
     vue,
@@ -11,23 +15,31 @@ module.exports = function(config) {
 }
 
 function generatorJS(config) {
-  // var components = config.components
-  // var needImportComponets = []
-  // // 
-  // components.forEach(item => {
-  //   var name = item.name
+  // vant ui 的不需引入。
+  var needImportComponents = config.selectedComponent.filter(c => {
+    return c.name.indexOf('van-') === -1
+  })
+  needImportComponents = uniqComponent(needImportComponents)
+  var importCode = needImportComponents
+    .map(c => {
+      return `
+import ${c.name} from '@/components/${c.name}'`
+    })
+    .join('')
 
-  //   if(needImportComponets.indexOf(name) === -1) {
-  //     needImportComponets.push(name)
-  //   }
-  // })
+  var useComponentCode = needImportComponents
+    .map(c => {
+      return `
+    ${c.name},`
+    })
+    .join('')
 
   var js = `
-// todo
-// import 
+${importCode}
+
 export default {
   components: {
-   // todo
+   ${useComponentCode}
   },
   data() {
     return {
@@ -45,9 +57,39 @@ export default {
 }
 
 function generatorVue(config) {
+  var componentsCode = config.selectedComponent.map(c => {
+    var code = `
+    <${c.name} v-bind="${handleProps(JSON.stringify(c.props, null, '\t'))}">
+      ${!c.slots ? '' : c.slots.map(item => {
+        let res = `
+      <template name="${item.name || 'default'}" >
+        ${!item.isList 
+          ? `
+        <div 
+          v-html="'${handleHTML(item.content)}'"
+        ></div>
+          `
+          : `
+          <component 
+            is="${item.componentName || 'div'}"
+            v-for="i in 3"
+            :key="i"
+            v-html="'${handleHTML(item.content)}'">
+          </component>
+          `
+        }
+      </template>`
+        return res
+      }).join('\n')}
+      
+    </${c.name}>
+`
+    return code
+  }).join('\n')
   var vue = `
 <template> 
   <div class="main">
+    ${componentsCode}
   </div>
 </template>
 <script src="./main.js"></script>
@@ -57,8 +99,29 @@ function generatorVue(config) {
 
 }
 
+
 function generatorCSS(config) {
   var css = `
 `
   return css
+}
+
+function handleHTML(html) {
+  return html.replace(/\"/g, '')
+}
+
+function handleProps(html) {
+  return html.replace(/\"/g, '\'')
+}
+
+function uniqComponent(arr) {
+  var res = []
+  var addCNames = {}
+  arr.forEach(c => {
+    if(!addCNames[c.name]) {
+      addCNames[c.name] = true
+      res.push(c)
+    }
+  })
+  return res
 }
